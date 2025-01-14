0fdceb99fd680aa3730b76b98e2dcd8fef99b327
Hassan-Raza-Shaikh <ammarbatman9@gmail.com>
2025-01-14 08:26:14 +0400

    .

---
+++ practice.py
@@ -0,0 +1,11 @@
+age = 20
+price = 19.5
+name = "Hassan"
+name_person = input("Enter your name: ")
+is_condition = True
+print("hello " + name_person)
+
+birth = input("Enter your birthyear: ")
+int(birth)
+age = 2025 - birth
+print(age)
